742e5a0dc024134a401ae6ea92403b5e57bd17e6
Spyros Amvrakidis <36534737+spyrosamvra@users.noreply.github.com>
2022-06-23 18:08:40 +0300

    Update var.providers.tf

--- var.providers.tf
+++ var.providers.tf
@@ -1,5 +1,5 @@
 variable "htoken" {
   type        = string
   description = "Hetzner cloud token"
-  default     = "ifHRU5EM5ctxEahhdAsgtutC8FM48z4R8pKoZwKcizkLkNfTRHqrcehJeJGxcgvv"
+  default     = ""
 }
